20a428a6fe9315776489c25cc970d6e09e000d49
Bartlomiej Chruslicki <b13chruslicki@gmail.com>
2023-02-05 20:44:36 +0100

    First commit

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Register as a coach now!</h2>
+            <coach-filter @save-data="saveData"></coach-filter>
+        </base-card>
+    </section>
+</template>
+
+<script>
+import CoachFilter from '../../components/coaches/CoachForm.vue';
+
+export default {
+    components: {
+        CoachFilter
+    },
+    methods: {
+        saveData(data) {
+            this.$store.dispatch('coaches/registerCoach', data);
+            this.$router.replace('/coaches');
+        }
+    }
+}
+</script>
